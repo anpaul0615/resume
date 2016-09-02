7d13ffe4219739d8548448b1954f3c44c7d7949f
Paul An <anpaul0615@gmail.com>
2016-09-02 16:45:06 +0900

    sync workspace

---
+++ _posts/newpost.bat
@@ -0,0 +1,11 @@
+@echo off
+
+:: 현재시각
+set now_date=%date%
+set now_hour=%time:~0,2%
+set now_min=%time:~3,2%
+set now_sec=%time:~6,2%
+set filename=%now_date%@%now_hour%-%now_min%-%now_sec%.md
+
+:: 템플릿파일 복사
+copy .\template.md .\%filename%
