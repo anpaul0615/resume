853995056664c1db46e99aaae4b20e226dcba0d8
Paul An <anpaul0615@gmail.com>
2017-01-11 13:59:42 +0900

    amend post template generate script (_posts/newpost.bat)

--- _posts/newpost.bat
+++ _posts/newpost.bat
@@ -1,11 +1,29 @@
-@echo off
+ï»¿@echo off
 
-:: ÇöÀç½Ã°¢
+# í˜„ìž¬ì‹œê°
 set now_date=%date%
 set now_hour=%time:~0,2%
 set now_min=%time:~3,2%
 set now_sec=%time:~6,2%
 set filename=%now_date%-%now_hour%-%now_min%-%now_sec%.md
 
-:: ÅÛÇÃ¸´ÆÄÀÏ º¹»ç
-copy .\template.md .\%filename%
+# í…œí”Œë¦¿íŒŒì¼ ìž‘ì„±
+@chcp 65001
+@echo --- >> .\%filename%
+@echo layout: base >> .\%filename%
+@echo category: "diray" >> .\%filename%
+@echo date: %now_date% %now_hour%:%now_min%:%now_sec%  >> .\%filename%
+@echo title: "hello" >> .\%filename%
+@echo tags: >> .\%filename%
+@echo --- >> .\%filename%
+
+@echo. >> .\%filename%
+@echo ### {{ page.title }} ^<small^>{{ page.date ^| date: "%%Y-%%m-%%d" }}^</small^> >> .\%filename%
+@echo --- >> .\%filename%
+@echo. >> .\%filename%
+
+@echo ^>hello >> .\%filename%
+@echo. >> .\%filename%
+
+@echo hello >> .\%filename%
+@echo hello >> .\%filename%
