89ed5eb8a015743fdb2c426cb8c6efbeff3d340a
Paul An <anpaul0615@gmail.com>
2017-01-12 10:33:05 +0900

    fix timestamp slicing script (_posts/newpost.bat)

--- _posts/newpost.bat
+++ _posts/newpost.bat
@@ -3,9 +3,13 @@
 # 현재시각
 set now_date=%date%
 set now_hour=%time:~0,2%
+if "%now_hour:~0,1%" == " " set now_hour=0%now_hour:~1,1%
 set now_min=%time:~3,2%
+if "%now_min:~0,1%" == " " set now_min=0%now_min:~1,1%
 set now_sec=%time:~6,2%
+if "%now_sec:~0,1%" == " " set now_sec=0%now_sec:~1,1%
 set filename=%now_date%-%now_hour%-%now_min%-%now_sec%.md
+
 
 # 템플릿파일 작성
 @chcp 65001
