34a7c24743498bf669103464c341e8160e131a76
Paul An <anpaul0615@gmail.com>
2017-01-31 22:17:59 +0900

    fix menu-activation

--- assets/javascripts/base.js
+++ assets/javascripts/base.js
@@ -1,10 +1,5 @@
-(function (window, document, $) {
-
+$(function(){
   // 현재메뉴 활성화
-  let current = $('#navbar').attr('data-active');
-  $('#navbar > ul > li').each(function(i,el){
-    let path = $(this).children('a').attr('href');
-    if(current == path) $(this).addClass('active');
-  });
-
-}(window, window.document, window.jQuery));
+  var current = document.location.pathname.split('/')[1];
+  $('#'+current).addClass('active');
+});
