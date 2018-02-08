9cb95fb97e2c6b34b77bdab782867d3c244d63ab
PAUL <anpaul0615@gmail.com>
2018-02-08 23:18:09 +0900

    Add introduce-section

--- app/components/View/Introduce.js
+++ app/components/View/Introduce.js
@@ -5,41 +5,78 @@ define(function(require, exports, module) {
     var Axios = require('axios');
     // Component Style
     var style = {
-        root: {}
+        root: {
+            display: 'flex',
+            width: '100%',
+            height: 'auto',
+            paddingBottom: '20px',
+            borderBottom: '1px solid #eee'
+        },
+        avatar: {
+            height: '100%',
+            position: 'absolute'
+        }
     };
     // Component
     return Introduce = createReactClass({
         displayName: 'Introduce',
         getInitialState: function() {
             return {
-                data: null
+                avatar: 'https://avatars0.githubusercontent.com/u/8488507?s=460&v=4',
+                name: 'Paul An',
+                whoami: 'An ordinary developer.',
+                links: []
             };
         },
         componentDidMount: function() {
-            // console.log('[Component] Introduce Mounted!!');
-            // console.log(this.props);
             var _this = this;
             Axios.get('/data/introduce.json')
             .then(function(response) {
                 if(response.status === 200) {
+                    var introduce = response.data;
                     _this.setState({
-                        data: JSON.stringify(response.data)
+                        avatar: introduce.avatar,
+                        name: introduce.name,
+                        whoami: introduce.whoami,
+                        links: introduce.links
                     });
                 }
             })
             .catch(function(err) {
                 console.log(err);
             });
-
         },
         render: function() {
-            return this.state.data
-            ? React.createElement(
-                'p', null, JSON.stringify(this.state.data)
+            return this.state.avatar
+            ?
+            React.createElement( 'div', { className: 'col-xs-12', style: style.root },
+                // introduce
+                React.createElement( 'div', { className: 'col-xs-12 col-sm-8 col-md-9' },
+                    // name
+                    React.createElement( 'h1', null, this.state.name ),
+                    React.createElement( 'blockquote', null, this.state.whoami ),
+                    // links
+                    React.createElement( 'ul', null, 
+                        this.state.links.map(function(link,idx){
+                            return React.createElement( 'li', { key: idx }, 
+                                React.createElement( 'a',
+                                    { 
+                                        href: link.url,
+                                        target: "_blank"
+                                    }, 
+                                    link.name
+                                )
+                            );
+                        })
+                    )
+                ),
+                // avatar
+                React.createElement( 'div', { className: 'col-xs-12 col-sm-4 col-md-3 hidden-xs' }, 
+                    React.createElement( 'img', { className: 'img-thumbnail', style: style.avatar, src: this.state.avatar } )
+                )
             )
-            : React.createElement(
-                'p', null, 'no-data'
-            );
+            :
+            React.createElement( 'p', null, 'no-data' );
         }
     });
 });
