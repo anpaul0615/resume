559c09402849441206be59686e8df63a4d080f23
PAUL <anpaul0615@gmail.com>
2018-02-21 00:07:08 +0900

    Add experience-section

--- app/components/View/Experience.js
+++ app/components/View/Experience.js
@@ -5,14 +5,20 @@ define(function(require, exports, module) {
     var Axios = require('axios');
     // Component Style
     var style = {
-        root: {}
+        root: {
+            width: '100%',
+            height: 'auto',
+            paddingBottom: '20px',
+            borderBottom: '1px solid #eee'
+        },
     };
     // Component
     return Experience = createReactClass({
         displayName: 'Experience',
         getInitialState: function() {
             return {
-                data: null
+                description: 'project experience loading..',
+                companies: []
             };
         },
         componentDidMount: function() {
@@ -22,8 +28,10 @@ define(function(require, exports, module) {
             Axios.get('/data/experience.json')
             .then(function(response) {
                 if(response.status === 200) {
+                    var experience = response.data;
                     _this.setState({
-                        data: JSON.stringify(response.data)
+                        description: experience.description,
+                        companies: experience.companies
                     });
                 }
             })
@@ -32,13 +40,41 @@ define(function(require, exports, module) {
             });
         },
         render: function() {
-            return this.state.data
-            ? React.createElement(
-                'p', null, JSON.stringify(this.state.data)
+            return this.state.companies.length > 0
+            ?
+            React.createElement( 'div', { className: 'col-xs-12', style: style.root },
+                // companies 
+                this.state.companies.map(function(company, company_idx){
+                    return [
+                        // company-info
+                        React.createElement( 'h3', { key: 'company-'+ company_idx },
+                            company.name,
+                            React.createElement( 'br', null ),
+                            React.createElement( 'small', null, company.job_title ),
+                            React.createElement( 'small', null, ', ' ),
+                            React.createElement( 'small', null, company.working_period )
+                        ),
+                        // projects
+                        React.createElement( 'ul', { key: 'company-'+ company_idx +'-projects' },
+                            company.projects.map(function(project, project_idx){
+                                return React.createElement( 'li', { key: 'company-'+ company_idx +'-projects-'+ project_idx }, 
+                                    React.createElement( 'a',
+                                        {
+                                            href: project.detail,
+                                            target: "_blank"
+                                        },
+                                        project.title_en
+                                    ),
+                                    React.createElement( 'br', null ),
+                                    React.createElement( 'small', null, project.description_en )
+                                );
+                            })
+                        )
+                    ];
+                })
             )
-            : React.createElement(
-                'p', null, 'no-data'
-            );
+            :
+            React.createElement( 'p', null, 'no-data' );
         }
     });
 });
